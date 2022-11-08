a7279d751328e934e85803d232c92abbb53c0394
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-11-07 20:37:57 -0800

    added starting logic

---
+++ Results/results.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  const city = $("#city");
+  const searchBtn = $("#placeholder");
+  const date = $("#date");
+  const radius = $("#radius");
+  const genreId = $("#genreSearch");
+
+  //add event handler for search button click
+  $("#submitButton").on("click", pullBands());
+
+  //render available options from api, hover over options display detail card
+  function pullBands(res) {
+    fetch(
+      `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&${date}&${radius}&${genreId}apikey=UrzgZnCWVNTGuF7NQOaiNiHxt2Kjh8AI`
+    )
+      .then((res) => res.json())
+      .then((res) => {});
+    renderBands(res);
+  }
+  // this will display our results in our card
+  function renderBands(bandsData) {
+    const city = bandsData.city;
+    const bands = bandsData.bands;
+    document.getElementById("band").textContent = bandsData.events.name;
+    document.getElementById("placeholder").innerHTML = "";
+    document.getElementById("city").textContent =
+      bandsData.events.venues.city.name;
+    document.getElementById("datetime").textContent =
+      bandsData.events.dates.start.dateTime;
+    console.log(band, city, datetime);
+  }
+});
--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,34 @@
-var weather = {
-  apiKey: "af0e82322edef7c54df70bf9d0e3082f",
-  fetchWeather: function () {
+$(document).ready(function () {
+  const city = $("#city").value; 
+  const searchBtn = $("#placeholder");
+  const date = $("#date").value 
+  const radius = $("#radius");
+  const genreId = $("#genreSearch").value 
+
+
+  //add event handler for search button click
+  $("#submitButton").on("click", pullBands());
+
+  //render available options from api, hover over options display detail card
+  function pullBands(res) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/forecast?q=Denver&appid=af0e82322edef7c54df70bf9d0e3082f"
+      `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&${date}&${radius}&${genreId}apikey=UrzgZnCWVNTGuF7NQOaiNiHxt2Kjh8AI`
     )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  },
-};
+      .then((res) => res.json())
+      .then((res) => {});
+    renderBands(res);
+  }
+  // this will display our results in our card
+  function renderBands(bandsData) {
+    const city = bandsData.city;
+    const bands = bandsData.bands;
+    document.getElementById("band").textContent = bandsData.events.name;
+    document.getElementById("placeholder").innerHTML = "";
+    document.getElementById("city").textContent = bandsData.events.venues.city.name;
+    document.getElementById("datetime").textContent =bandsData.events.dates.start.dateTime;
+    console.log(band, city, datetime);
+  }
+});
+
 
 
