d6ebed7c0a7b648296de1c5bbfcec26475d13a8a
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-11-08 20:07:05 -0800

    refactored js

--- Results/results.js
+++ Results/results.js
@@ -1,32 +1,39 @@
 $(document).ready(function () {
-  const city = $("#city");
-  const searchBtn = $("#placeholder");
-  const date = $("#date");
-  const radius = $("#radius");
-  const genreId = $("#genreSearch");
+  const city = "Irvine";
+  const searchBtn = $("#searchBtn");
+  var timeclass = $('.time');
+  const genreId = "rock";
 
   //add event handler for search button click
-  $("#submitButton").on("click", pullBands());
-
+  searchBtn.on("click", pullBands);
+  pullBands();
   //render available options from api, hover over options display detail card
   function pullBands(res) {
+    //TODO - findout correct format for genreId param and startDateTime param -
     fetch(
-      `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&${date}&${radius}&${genreId}apikey=UrzgZnCWVNTGuF7NQOaiNiHxt2Kjh8AI`
+      //TODO: validate options before fetching
+      `https://app.ticketmaster.com/discovery/v2/events.json?city=${city.val()}&startDateTime=${date.val()}:00Z&radius=100&apikey=UrzgZnCWVNTGuF7NQOaiNiHxt2Kjh8AI`
     )
       .then((res) => res.json())
-      .then((res) => {});
-    renderBands(res);
+      .then((res) => {
+        renderBands(res);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
   }
   // this will display our results in our card
   function renderBands(bandsData) {
-    const city = bandsData.city;
-    const bands = bandsData.bands;
-    document.getElementById("band").textContent = bandsData.events.name;
-    document.getElementById("placeholder").innerHTML = "";
-    document.getElementById("city").textContent =
-      bandsData.events.venues.city.name;
-    document.getElementById("datetime").textContent =
-      bandsData.events.dates.start.dateTime;
-    console.log(band, city, datetime);
+    console.log(bandsData, "<----- bands data");
+    //TODO - after getting the bands data, work on code below
+    // const city = bandsData.city;
+    // const bands = bandsData.bands;
+    // document.getElementById("band").textContent = bandsData.events.name;
+    // document.getElementById("placeholder").innerHTML = "";
+    // document.getElementById("city").textContent =
+    //   bandsData.events.venues.city.name;
+    // document.getElementById("datetime").textContent =
+    //   bandsData.events.dates.start.dateTime;
+    // console.log(band, city, datetime);
   }
 });
