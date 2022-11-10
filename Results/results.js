d7a959c8b48015f9495e7eaa6cc955363e9f3625
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2022-11-10 10:58:19 -0800

    added googlemap api

--- Results/results.js
+++ Results/results.js
@@ -146,3 +146,14 @@ $(document).ready(function () {
       });
   }
 });
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
